2a3244dcd8c7e32a7580184ff02fc79156834e9b
Saeth Wannasuphoprasit <58103064+saeth40@users.noreply.github.com>
2021-12-30 02:10:59 +0000

    Add files via upload

---
+++ Preparing test and train dataset/Dividing_500.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data=pd.read_csv('C:\\Users\\saeth\\Desktop\\TDRI\\test_nonStemV2_50000.csv')
+print(data.columns)
+div=500  # division
+num_people=10
+left=500  # slicing index
+for i in range(2,num_people+1):  # i = 1,2,3,..., num_people
+    select=data[left:i*div]  # selected rows
+    select['job_label']=pd.Series()  # add 'job_label' column
+    print('i=',i)
+    print(left,i*div)
+    select.to_csv('C:\\Users\\saeth\\Desktop\\TDRI\\csv_5000\\test_5000_no'+str(i-1)+'.csv',index=False, header=True)
+    left=i*div  # update slicing index
